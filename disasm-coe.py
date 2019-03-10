64bc222651573a511e36588b468727c34c5aed5c
Xiaoguang Zhu <694066681@qq.com>
2019-03-10 15:40:49 +0800

    Add files via upload

--- disasm-coe.py
+++ disasm-coe.py
@@ -1,6 +1,37 @@
 # -*- coding: utf-8 -*-
 import capstone as cs
 
+'''
+#### `.coe` File Format Example
+
+```text
+memory_initialization_radix=16;
+memory_initialization_vector=
+3C10BFC0 00000000 00000000 00000000
+36100000 00000000 00000000 00000000
+BE090010 00000000 00000000 00000000
+8E110000 00000000 00000000 00000000
+00000000 00000000 00000000 00000000
+00000000 00000000 00000000 00000000
+00000000 00000000 00000000 00000000
+00000000 00000000 00000000 00000000
+;
+```
+
+> Note:  
+> - The leading two lines are required!
+> - One instruction word per line only!
+> - Instruction words must be found in the first column!
+> - Instruction words are in __BIG ENDIAN__!
+'''
+
+######## Configurations - CHANGE ME !!! ########
+
+# path to `.coe` file to be disassemblied
+filename = './ram_init_os.coe'
+
+
+######## Helper Functions ########
 
 def chr_to_num(in_: str):
     if len(in_) != 1:
@@ -30,30 +61,49 @@ def word_to_intlist(in_: str):
 
 ######### Main #########
 
-filename = './ram_init_os.coe'
 bytelist = []   # list of int
 with open(filename, 'r') as coefile:
     # skip file property lines
     coefile.readline()
     coefile.readline()
-    # parse __FORMATTED__ vector
     for lineno, line in enumerate(coefile):
-        # print('processing line No.{}'.format(lineno), end='\r')
+        if line.strip() == ';':
+            break
         try:
             bytelist.extend(word_to_intlist(line.split(' ')[0]))
         except ValueError as e:
-            # print('Caught ValueError: ', end='')
-            # print(e)
+            print('Caught ValueError: {}'.format(e))
             break
     print('')
 
 md = cs.Cs(cs.CS_ARCH_MIPS, cs.CS_MODE_MIPS32 + cs.CS_MODE_BIG_ENDIAN)
 md.mnemonic_setup(cs.mips.MIPS_INS_AUI, 'lui')
 code = bytes(bytelist)
+
 print('Address     Word       Instruction')
 print('=======     ====       ===========')
+print('')
+
+__guess_seg_end = False     # static variable of `likely_segment_end()`
+def likely_segment_end(mne: str):
+    '''
+    It is naiively believed that on a platform with delay slot, unconditional
+    branching / jumping followed by an empty delay slot, i.e. `nop`, is likely
+    indicating a `goto` / `return` statement in disassembly.
+
+    Inserting an empty line in the output is definitely good for your eyes.
+    '''
+    global __guess_seg_end
+    if (mne[0] in 'j') or (mne in ['b', 'sdbbp', 'eret']):
+        __guess_seg_end = True
+    elif __guess_seg_end and mne == 'nop':
+        __guess_seg_end = False
+        return True
+    return False
 
 for instr in md.disasm(code, 0xbfc00000):
     print('0x{:8x}: {:8s}   {:<7s}{:s}'.format(
         instr.address, instr.bytes.hex(), instr.mnemonic, instr.op_str
     ))
+    if likely_segment_end(instr.mnemonic):
+        print('')
